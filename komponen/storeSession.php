3eb72f3ead3d2bb8696fb1422b4975c67aa02a2f
afifatulmukaromah <fifahaf742@gmail.com>
2025-05-26 01:04:39 +0700

    upload Store Session

--- komponen/storeSession.php
+++ komponen/storeSession.php
@@ -1,3 +1,14 @@
 <?php
 session_start();
 $data = json_decode(file_get_contents("php://input"), true);
+
+$_SESSION['checkout'] = [
+    'nama' => $data['nama'],
+    'email' => $data['email'],
+    'telp' => $data['telp'],
+    'alamat' => $data['alamat'],
+    'pengiriman' => $data['pengiriman'],
+    'ongkir' => (int) $data['ongkir'],
+    'subtotal' => (int) $data['subtotal'],
+    'total' => (int) $data['total']
+];
